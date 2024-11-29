b0516789a9156d120c1ff31bc3d33998066d573d
jeffzi <16199817+jeffzi@users.noreply.github.com>
2024-11-29 15:20:03 +0100

    feat: use homebrew to install chezmoi

--- install.sh
+++ install.sh
@@ -52,13 +52,7 @@ if [ ! "$(command -v chezmoi)" ]; then
   bin_dir="$HOME/.local/bin"
   chezmoi="$bin_dir/chezmoi"
   info "Installing chezmoi..."
-  if [ "$(command -v curl)" ]; then
-    sh -c "$(curl -fsSL https://git.io/chezmoi)" -- -b "$bin_dir"
-  elif [ "$(command -v wget)" ]; then
-    sh -c "$(wget -qO- https://git.io/chezmoi)" -- -b "$bin_dir"
-  else
-    error "To install chezmoi, you must have curl or wget installed."
-  fi
+  brew install chezmoi
 else
   chezmoi=chezmoi
 fi
