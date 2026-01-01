03d196570e730d78c1948875f9636b44445148e0
jeffzi <16199817+jeffzi@users.noreply.github.com>
2026-01-01 09:36:38 +0100

    fix(fish): add cargo bin to PATH
    Add ~/.cargo/bin to fish PATH configuration to enable cargo command.

--- home/dot_config/fish/conf.d/00-paths.fish
+++ home/dot_config/fish/conf.d/00-paths.fish
@@ -1,6 +1,7 @@
 # PATH setup - loaded first to ensure tools are available
 fish_add_path /opt/homebrew/bin
 fish_add_path ~/.local/bin
+fish_add_path ~/.cargo/bin
 fish_add_path ~/go
 
 # uv-managed Python 3.8 for legacy poetry projects
