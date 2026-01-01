19739012cc35757f6ec5ae4b0045d82a4d12c321
jeffzi <16199817+jeffzi@users.noreply.github.com>
2026-01-01 11:07:27 +0100

    refactor(fish): remove fish_variables.tmpl, use conf.d instead
    Move EZA options to 10-env.fish and let fish manage fish_variables
    itself. This avoids conflicts between chezmoi and fish's universal
    variable management.

--- home/dot_config/fish/conf.d/10-env.fish
+++ home/dot_config/fish/conf.d/10-env.fish
@@ -1,6 +1,12 @@
 # Environment variables
 set -gx EDITOR (which code)
 
+# eza options
+set -gx EZA_STANDARD_OPTIONS -la -F --icons --group-directories-first --time-style=long-iso --git -o
+set -gx EZA_LL_OPTIONS -la -F --icons --group-directories-first --time-style=long-iso --git
+set -gx EZA_LS_OPTIONS -la -F --icons --group-directories-first --time-style=long-iso --no-permissions --no-user
+set -gx EZA_LT_OPTIONS --tree --no-permissions --no-user
+
 # Project paths for pj plugin
 # https://github.com/oh-my-fish/plugin-pj
 set -gx PROJECT_PATHS ~/Projects ~/Projects/personal ~/Projects/work/metamoki ~/Projects/work/metamoki/data-engineering/projects ~/Projects/work/metamoki/data-engineering/libs
