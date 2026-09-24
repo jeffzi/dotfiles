7066d23395b4e9c6e543c08f7309ea2b7a6a8e72
jeffzi <16199817+jeffzi@users.noreply.github.com>
2026-09-24 14:18:16 +0200

    fix(fish): add ~/go/bin to PATH

--- home/dot_config/fish/conf.d/00-paths.fish
+++ home/dot_config/fish/conf.d/00-paths.fish
@@ -2,12 +2,4 @@
 fish_add_path /opt/homebrew/bin
 fish_add_path ~/.local/bin
 fish_add_path ~/.cargo/bin
-fish_add_path ~/go
-
-# uv-managed Python 3.8 for legacy poetry projects
-if command -q uv
-    set -l py38_path (uv python find 3.8 2>/dev/null | xargs dirname)
-    if test -n "$py38_path"
-        fish_add_path --path $py38_path
-    end
-end
+fish_add_path ~/go/bin
