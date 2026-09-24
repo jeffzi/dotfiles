fb671b050240074a20285668bb1952825c477a25
jeffzi <16199817+jeffzi@users.noreply.github.com>
2026-09-24 11:03:36 +0200

    feat(chezmoi): prompt for the work org directory

--- home/dot_config/fish/functions/ll.fish
+++ home/dot_config/fish/functions/ll.fish
@@ -1,3 +1,3 @@
-function ll --wraps='eza $EZA_STANDARD_OPTIONS' --description "Use eza and its git options if in a git repo"
+function ll --wraps='eza' --description "Use eza and its git options if in a git repo"
     command eza -laF --icons --group-directories-first --time-style=long-iso --git $argv
 end
